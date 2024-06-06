e3e1cdf0a3e3f47b65671efb8849301a7f62e159
vasmohi <mohit.vashistha@aelf.io>
2024-06-06 04:24:54 +0000

    Added tutorial changes

---
+++ lottery_game/src/ContractReferences.cs
@@ -0,0 +1,9 @@
+using AElf.Contracts.MultiToken;
+
+namespace AElf.Contracts.LotteryGame
+{
+    public partial class LotteryGameState
+    {
+        internal TokenContractContainer.TokenContractReferenceState TokenContract { get; set; }
+    }
+}
---
+++ lottery_game/test/_Setup.cs
@@ -0,0 +1,32 @@
+﻿using AElf.Cryptography.ECDSA;
+using AElf.Testing.TestBase;
+using ContractNamespacePlaceholder;
+
+namespace AElf.Contracts.HelloWorld
+{
+    // The Module class load the context required for unit testing
+    public class Module : ContractTestModule<LotteryGame>
+    {
+        
+    }
+    
+    // The TestBase class inherit ContractTestBase class, it defines Stub classes and gets instances required for unit testing
+    public class TestBase : ContractTestBase<Module>
+    {
+        // The Stub class for unit testing
+        internal readonly LotteryGameContainer.LotteryGameStub HelloWorldStub;
+        // A key pair that can be used to interact with the contract instance
+        private ECKeyPair DefaultKeyPair => Accounts[0].KeyPair;
+
+        public TestBase()
+        {
+            HelloWorldStub = GetContractStub(DefaultKeyPair);
+        }
+
+        private LotteryGameContainer.LotteryGameStub GetContractStub(ECKeyPair senderKeyPair)
+        {
+            return GetTester<LotteryGameContainer.LotteryGameStub>(ContractAddress, senderKeyPair);
+        }
+    }
+    
+}
